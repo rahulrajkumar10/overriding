efd447ee1c7e8292e6be17ff44329caa86b4c16b
Tharun <Tharun@ROGG>
2024-03-11 14:25:29 +0530

    initial

---
+++ assignment2/src/assignment2/Shape.java
@@ -0,0 +1,14 @@
+package assignment2;
+
+public class Shape {
+
+	public Shape() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
